f1dec99b86d5e335b9d64f9dd3a6e5470d05c64e
CharlyMora <morandres.carlos@gmail.com>
2021-03-10 15:20:33 -0500

    test

--- modules/src/main/java/com/platzi/functional/_04_functional/CLIArgumentsUtils.java
+++ modules/src/main/java/com/platzi/functional/_04_functional/CLIArgumentsUtils.java
@@ -1,4 +1,14 @@
 package com.platzi.functional._04_functional;
 
+import java.util.function.Consumer;
+
 public class CLIArgumentsUtils {
+    static void showHelp(CLIArguments cliArguments){
+        Consumer<CLIArguments> consumerHelper = cliArguments1 -> {
+          if(cliArguments1.isHelp()){
+              System.out.println("Manual solicitado");
+          }
+        };
+        consumerHelper.accept(cliArguments);
+    }
 }
